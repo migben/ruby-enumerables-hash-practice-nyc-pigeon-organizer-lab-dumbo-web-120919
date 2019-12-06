8507ad6d97b0ac5b9cdb946b5b30af0660d537a0
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:53:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+=begin
+reference hash of pigeon_data
+keep it in mind
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
+=end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
   a_hash = {}
