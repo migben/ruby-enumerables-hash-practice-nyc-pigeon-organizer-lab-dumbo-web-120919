a87011518880c89b0024a0ab8a4fa66b7fa70cae
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:50:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  a_hash = {}
+  
+  data.each do |k, v|
+    v.each do |new_v, name|
+      name.each do |x|
+        
+        if !a_hash[x]
+          
+      end
+  
+    end
+    
+  end
+  
 end
